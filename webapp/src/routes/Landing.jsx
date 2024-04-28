8896c1b9dfb16af666f86a4e806661f9479c74c2
Dorine Andreasson <dorine18@outlook.com>
2024-04-28 18:55:45 +0200

    changed to  landing

---
+++ webapp/src/routes/Landing.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Landing from './Landing.';
+
+
+
+       
+
+const Landing = () => ( 
+   
+        <div>
+            <h1> Introduction to our products</h1>
+            <Landing/>
+
+        </div>
+       
+
+    )
+
+export default Landing;
